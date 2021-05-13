9e6e135efbb564ef4e8b677777bd4cb03025ff6e
Loule | Louis <35641311+Loule95450@users.noreply.github.com>
2021-05-13 13:34:05 +0200

    Welcome on repo name

--- app/config/config.php
+++ app/config/config.php
@@ -15,14 +15,14 @@
 	
 	// URL ROOT (Liens dynamiques)
     if(strpos($_SERVER['HTTP_HOST'], "localhost") !== false || strpos($_SERVER['HTTP_HOST'], "127.0.0.1") !== false){
-        define('URL_ROOT', "http://" . $_SERVER['HTTP_HOST'] . str_replace("public/index.php", "", $_SERVER['SCRIPT_NAME']));
+        define('URL_ROOT', "http://" . $_SERVER['HTTP_HOST'] . str_replace("/public/index.php", "", $_SERVER['SCRIPT_NAME']));
     } else {
-        define('URL_ROOT', "https://".$_SERVER['HTTP_HOST'].str_replace("public/index.php", "", $_SERVER['SCRIPT_NAME']));
+        define('URL_ROOT', "https://".$_SERVER['HTTP_HOST'].str_replace("/public/index.php", "", $_SERVER['SCRIPT_NAME']));
     }
     //define('URL_ROOT', 'https://MyWebsite.com');
 
 	// Nom du site
-	define('SITE_NAME', '=WebsiteName=');
+	define('SITE_NAME', str_replace("/public/index.php", "", $_SERVER['SCRIPT_NAME']));
 
 	//Meta
     define('CARD_DESCRIPTION', '=WebsiteDescription=');
