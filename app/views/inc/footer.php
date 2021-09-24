544682d5116021eb1019c6b94ceede5a19f5d89b
Loule | Louis <83773670-2bf4-4f80-9324-8c4bdf950ad0@loule.me>
2021-09-24 13:06:01 +0200

    🐞 - Add an "alt" attribute to this image.

--- app/views/inc/footer.php
+++ app/views/inc/footer.php
@@ -1,5 +1,5 @@
 <footer>
-    <p>Template by Loule <img src="https://avatars.githubusercontent.com/u/35641311" width="24px"> <a class="github-button" href="https://github.com/Loule95450/PHP-MVC-Template" data-color-scheme="no-preference: light; light: dark_dimmed; dark: dark_dimmed;" data-icon="octicon-star" data-size="large" data-show-count="true" aria-label="Star Loule95450/PHP-MVC-Template on GitHub">Star</a></p>
+    <p>Template by Loule <img src="https://avatars.githubusercontent.com/u/35641311" alt="Loule95450 Profile" width="24px"> <a class="github-button" href="https://github.com/Loule95450/PHP-MVC-Template" data-color-scheme="no-preference: light; light: dark_dimmed; dark: dark_dimmed;" data-icon="octicon-star" data-size="large" data-show-count="true" aria-label="Star Loule95450/PHP-MVC-Template on GitHub">Star</a></p>
 </footer>
 
 <?php
